239f6553c9f393bc8a25524b17e2f28e7b8bdbd1
rico <vingkang@gmail.com>
2015-11-18 22:29:05 +0800

    Update demo.go

--- demo.go
+++ demo.go
@@ -16,7 +16,6 @@ a := "((1 -3 +3*4)+5 > 100) && (2<3)"
 a = " (2<3 || 2<1) && 3 <4"
 
 
-rtrue := big.NewRat(0,1)
 
 
 result, err := evaluator.Eval(a)
@@ -25,7 +24,7 @@ result, err := evaluator.Eval(a)
 ret, err := evaluator.Evaluate(a)
 fmt.Println(ret,2222222222222222222)
 
-fmt.Println(result,rtrue,777, evaluator.BigratToBool(result), err)
+fmt.Println(result,777, evaluator.BigratToBool(result), err)
 
 
 }
